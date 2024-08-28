4bb624c91dcd6cb0e6137775cd41c20b4ef0447c
S98082620090826 <s98082620090826@gmail.com>
2024-08-28 16:03:56 +0800

    更新 get_odds.py

--- get_odds.py
+++ get_odds.py
@@ -1,11 +1,11 @@
 def get_odds(a):
-	result={} #{}是字典的意思,詳細用法在我給你的那本書的p2-7有
-	n=0
-	for i in range(a):
-		if i%2==1:
-			n+=1 #n在這裡表示第n個奇數
-			result[n]=i #將字典中“鍵”的值設為i,表示第n個奇數是i
-	return result
+    result={} #{}是字典的意思,詳細用法在我給你的那本書的p2-7有
+    n=0
+    for i in range(a):
+        if i%2==1:
+            n+=1 #n在這裡表示第n個奇數
+            result[n]=i #將字典中“鍵”的值設為i,表示第n個奇數是i
+    return result
 
 out=get_odds(10) #會得到一個字典{ 1:1, 2:3, 3:54:7,5:9}
 print(out[3])
